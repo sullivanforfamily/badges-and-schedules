261c8a2118dde7a942d7b95727d45e2758ae73eb
charlessullivan <sullivanforfamily@gmail.com>
2021-01-19 00:18:27 -0800

    commit tree 5f3c84faadd2a98c56d37f561ae671a1b4d51bf4

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def badge_maker(name)
+    puts "Hello, my name is #{name}."
+end
+badge_maker
+
+
+
+def batch_badge_creator(array)
+    x = 0
+    while x <= "#{array.length}" do|array|
+        badge_maker(array.index)
+        x += 1
+    end
+end
+
+def assign_rooms(speaker)
+    room = [1,2,3,4,5,6,7]
+    x = 0
+    while x <= #{speaker.length}
+        puts " Hello #{speaker.each} You'll be assigned to room #{room.with_index}!"
+        x += 1
+    end
+end
+
+def printer
+    print (#{batch_badge_creator}, #{assign_rooms})
+end
+
+
